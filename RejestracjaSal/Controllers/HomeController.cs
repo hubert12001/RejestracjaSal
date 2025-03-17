9bab3ec1596f62c3e9a45b035c663964197fe99e
Hubert <hbadziag10473@gmail.com>
2025-03-17 16:42:15 +0100

    jest dobrze :-)

--- RejestracjaSal/Controllers/HomeController.cs
+++ RejestracjaSal/Controllers/HomeController.cs
@@ -24,5 +24,10 @@ namespace RejestracjaSal.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult StaticSites(string name)
+        {
+            return View(name);
+        }
     }
 }
