110a1bb5827a53d0e372259fbe4faee614c1e3dd
GiDaw <driftkompas@wp.pl>
2025-03-25 13:05:17 +0100

    cookies

--- RejestracjaSal/Controllers/HomeController.cs
+++ RejestracjaSal/Controllers/HomeController.cs
@@ -1,11 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using RejestracjaSal.Models;
 using System.Diagnostics;
+using System.Xml.Linq;
 
 namespace RejestracjaSal.Controllers
 {
     public class HomeController : Controller
     {
+
+        CookieOptions options = new CookieOptions()
+        {
+            Domain = "localhost", // Set the domain for the cookie
+            Path = "/", // Cookie is available within the entire application
+            Expires = DateTime.Now.AddDays(7), // Set cookie expiration to 7 days from now
+            Secure = false, // Ensure the cookie is only sent over HTTPS (set to false for local development)
+            HttpOnly = false, // Prevent client-side scripts from accessing the cookie
+            IsEssential = true // Indicates the cookie is essential for the application to function
+        };
+
+
         private readonly ILogger<HomeController> _logger;
         
         AppDbContext AppDbContext;
@@ -25,8 +38,26 @@ namespace RejestracjaSal.Controllers
             IQueryable<Users> myUsers = from user in AppDbContext.Users
                                        where user.Login == login && user.Password == password
                                        select user;
+
             if (myUsers.Any())
             {
+                var query = from Rooms in AppDbContext.Rooms
+                            join RoomTypes in AppDbContext.RoomTypes on Rooms.Type_id equals RoomTypes.Type_id
+                            join Locations in AppDbContext.Locations on Rooms.Location_id equals Locations.Location_id
+                            select new
+                            {
+                                name = Rooms.Name,
+                                price = Rooms.Room_price,
+                                capacity = Rooms.Capacity,
+                                description = Rooms.Description,
+                                image = Rooms.Image,
+                                type = RoomTypes.Name,
+                                location = Locations.Name,
+                            };
+                Response.Cookies.Append("login", login, options);
+
+                ViewBag.name = login;
+                ViewBag.Rooms = query;
                 return View("StronaGlowna");
             }
             else
@@ -40,6 +71,8 @@ namespace RejestracjaSal.Controllers
                                       string email, string username, string phone)
         {
             Users newUser;
+
+
 
             if (password== repeatPassword)
             {
@@ -66,8 +99,32 @@ namespace RejestracjaSal.Controllers
                     };
                 }
 
+                var query = from Rooms in AppDbContext.Rooms
+                            join RoomTypes in AppDbContext.RoomTypes on Rooms.Type_id equals RoomTypes.Type_id
+                            join Locations in AppDbContext.Locations on Rooms.Location_id equals Locations.Location_id
+                            select new
+                            {
+                                name = Rooms.Name,
+                                price = Rooms.Room_price,
+                                capacity = Rooms.Capacity,
+                                description = Rooms.Description,
+                                image = Rooms.Image,
+                                type = RoomTypes.Name,
+                                location = Locations.Name,
+                            };
+
+
                 AppDbContext.Users.Add(newUser);
                 AppDbContext.SaveChanges();
+
+                if (string.IsNullOrEmpty(HttpContext.Session.GetString(login)))
+                {
+                    HttpContext.Session.SetString(login, username);
+                }
+
+
+                ViewBag.Rooms = query;
+                Response.Cookies.Append("login", login, options);
                 return View("StronaGlowna");
             }
 
@@ -75,6 +132,14 @@ namespace RejestracjaSal.Controllers
             return View("Rejestracja");
             
         }
+
+        public IActionResult Logout()
+        {
+            Response.Cookies.Delete("login", options);
+            return View("StronaGlowna");
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
@@ -84,7 +149,6 @@ namespace RejestracjaSal.Controllers
 
         public IActionResult StaticSites(string name)
         {
-            List<Rooms> rooms = AppDbContext.Rooms.ToList();
 
             var query = from Rooms in AppDbContext.Rooms
                         join RoomTypes in AppDbContext.RoomTypes on Rooms.Type_id equals RoomTypes.Type_id
@@ -99,6 +163,9 @@ namespace RejestracjaSal.Controllers
                             type = RoomTypes.Name,
                             location = Locations.Name,
                         };
+
+
+
             if (name == "StronaGlowna")
             {
                 ViewBag.Rooms = query;
